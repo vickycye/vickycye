55913530edc6113d03d98ba625ceb9ffb036f6c5
Vicky Ye <vickyye@uw.edu>
2024-12-11 22:19:35 -0800

    added some blog items

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,12 @@ module.exports = {
       'solid-orange-10' : '#ffa530',
       'discord-gray' : '#1a1a1a',
       'discord-lighter-gray' : '#28292b',
-      'white' : '#ffffff'
+      'lighter-ish-gray' : '#424242',
+      'off-white' : '#e6e6e6',
+      'cream' : '#f0e6cc',
+      'white' : '#ffffff',
+      'tan-brown' : '#a86f51',
+      'pinkish-red' : '#d63c58'
     }
   },
   plugins: [],
