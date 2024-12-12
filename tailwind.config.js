dc34752f883d8507ffcb3e6901f5f06df004cc77
Vicky Ye <vickyye@uw.edu>
2024-12-11 17:41:09 -0800

    what the hell even happened since the last commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'light-orange-10' : '#ffd36b',
+      'solid-orange-10' : '#ffa530',
+      'discord-gray' : '#1a1a1a',
+      'discord-lighter-gray' : '#28292b',
+      'white' : '#ffffff'
+    }
+  },
+  plugins: [],
+}
+
