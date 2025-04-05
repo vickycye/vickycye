2e424a11b66d4a31e16f402dd864d65c87b46f1d
Vicky <93725266+vickycye@users.noreply.github.com>
2025-04-05 13:26:21 -0700

    Delete portfolio-website directory

--- portfolio-website/src/app/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Blog() {
-    return (
-      <div className="mt-24 px-4 py-12 max-w-screen-lg mx-auto">
-        <h1 className="text-4xl font-bold text-cream">Blog</h1>
-        <p className="mt-6 text-cream">Coming soon...</p>
-      </div>
-    );
-  }
