658e5b81bbb58a6668541ee210415e3a179577e3
Vicky Ye <vickyye@uw.edu>
2025-04-14 11:51:44 -0700

    deleted  a single line of code

--- portfolio-website/src/lib/markdown.ts
+++ portfolio-website/src/lib/markdown.ts
@@ -2,7 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import { remark } from 'remark';
-// import html from 'remark-html';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkRehype from 'remark-rehype';
